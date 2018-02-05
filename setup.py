023e2954e9772f327f655027abddff6edd2e77f7
zwennesm <martijn.zwennes@debijenkorf.nl>
2018-02-05 16:52:16 +0100

    bump to version 0.2

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='aws-video-transcoder',
-      version='0.1',
+      version='0.2',
       description='Transcodes videos to web-proof formats such as webp.',
       url='https://github.com/zwennesm/aws-video-transcoder',
       author='Martijn Zwennes',
